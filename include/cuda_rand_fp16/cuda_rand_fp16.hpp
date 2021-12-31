6ba9968890f97d3f22e5dd13fb4bcba29cab930c
mutsuki <mutsuki@momo86.net>
2021-12-31 15:01:02 +0900

    Add include/cuda_rand_fp16/cuda_rand_fp16.hpp

---
+++ include/cuda_rand_fp16/cuda_rand_fp16.hpp
@@ -0,0 +1,29 @@
+#ifndef __CUDA_RAND_FP16__
+#define __CUDA_RAND_FP16__
+#include <cstdint>
+#include <curand.h>
+#include <cuda_fp16.h>
+
+namespace mtk {
+namespace cuda_rand_fp16 {
+template <curandRngType_t rng>
+struct curand_status_t {using type = void;};
+template <> struct curand_status_t<CURAND_RNG_PSEUDO_PHILOX4_32_10> {using type = curandStatePhilox4_32_10_t;};
+
+struct generator_t {
+	unsigned num_sm;
+	unsigned num_threads;
+	cudaStream_t cuda_stream;
+	curandRngType_t rng_type;
+	void* status_ptr;
+};
+
+void create(generator_t& gen, const curandRngType_t rng_type);
+void destroy(generator_t& gen);
+void set_seed(generator_t& gen, const std::uint64_t seed);
+
+// Uniform rand distribution
+void uniform(generator_t& gen, half* const ptr, const std::size_t size);
+} // namespace cuda_rand_fp16
+} // namespace mtk
+#endif // __CUDA_RAND_FP16__
