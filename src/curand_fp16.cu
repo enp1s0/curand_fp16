ae22b953f24ab2db1d69a8232d49dab4076d5cea
mutsuki <mutsuki@momo86.net>
2022-01-19 21:44:13 +0900

    Add genblock-level-batch

--- src/curand_fp16.cu
+++ src/curand_fp16.cu
@@ -3,6 +3,10 @@
 
 namespace {
 constexpr unsigned block_size = 512;
+constexpr unsigned store_block_batch_size = 1;
+constexpr unsigned num_sm_scale = 8;
+using block_t = ulong2;
+
 template <class T>
 __global__ void status_init_kernel(
 		T* const status_ptr,
@@ -17,16 +21,17 @@ struct size_of{int value = 0;};
 template <> struct size_of<ushort1> {static const int value = 2;};
 template <> struct size_of<uint1  > {static const int value = 4;};
 template <> struct size_of<ulong2 > {static const int value = 16;};
+template <> struct size_of<ulong1 > {static const int value = 8;};
 template <> struct size_of<half   > {static const int value = 2;};
 template <> struct size_of<half2  > {static const int value = 4;};
 
-template <class RNG_T, uint32_t pm = 0>
+template <class RNG_T, class BLOCK_T, uint32_t pm = 0>
 __global__ void generate_kernel(
 		half* const array_ptr,
 		RNG_T* const status_ptr,
 		const std::size_t size
 		) {
-	const auto batch_size = size_of<ulong2>::value / size_of<half>::value;
+	const auto batch_size = size_of<BLOCK_T>::value / size_of<half>::value * store_block_batch_size;
 	const auto tid = blockDim.x * blockIdx.x + threadIdx.x;
 	auto curand_gen = *(status_ptr + tid);
 
@@ -34,26 +39,34 @@ __global__ void generate_kernel(
 	for (unsigned i = tid * batch_size; i < batch_loop_size; i += batch_size * gridDim.x * blockDim.x) {
 		// block gen
 		union {
-			half   h1[size_of<ulong2>::value / size_of<half >::value];
-			half2  h2[size_of<ulong2>::value / size_of<half2>::value];
-			ulong2 ul2;
-			unsigned u[size_of<ulong2>::value / size_of<uint1>::value];
-			short s[size_of<ulong2>::value / size_of<ushort1>::value];
-		} batch_block;
+			half   h1[size_of<BLOCK_T>::value / size_of<half >::value];
+			half2  h2[size_of<BLOCK_T>::value / size_of<half2>::value];
+			BLOCK_T store_block;
+			unsigned u[size_of<BLOCK_T>::value / size_of<uint1>::value];
+			short s[size_of<BLOCK_T>::value / size_of<ushort1>::value];
+		} batch_block[store_block_batch_size];
 
-		for (unsigned j = 0; j < size_of<ulong2>::value / size_of<half2>::value; j++) {
-			batch_block.u[j] = curand(&curand_gen);
-			if constexpr (pm == 0) {
-				batch_block.u[j] &= 0x7fff7fffu;
+		for (unsigned sb = 0; sb < store_block_batch_size; sb++) {
+			for (unsigned j = 0; j < size_of<BLOCK_T>::value / size_of<half2>::value; j++) {
+				batch_block[sb].u[j] = curand(&curand_gen);
+				if constexpr (pm == 0) {
+					batch_block[sb].u[j] &= 0x7fff7fffu;
+				}
 			}
 		}
-		for (unsigned j = 0; j < size_of<ulong2>::value / size_of<half>::value; j++) {
-			batch_block.h1[j] = __short2half_rn(batch_block.s[j]);
+		for (unsigned sb = 0; sb < store_block_batch_size; sb++) {
+			for (unsigned j = 0; j < size_of<BLOCK_T>::value / size_of<half>::value; j++) {
+				batch_block[sb].h1[j] = __short2half_rn(batch_block[sb].s[j]);
+			}
 		}
-		for (unsigned j = 0; j < size_of<ulong2>::value / size_of<half2>::value; j++) {
-			batch_block.h2[j] = __hmul2(batch_block.h2[j], __float2half2_rn(1.f / 0x7fff));
+		for (unsigned sb = 0; sb < store_block_batch_size; sb++) {
+			for (unsigned j = 0; j < size_of<BLOCK_T>::value / size_of<half2>::value; j++) {
+				batch_block[sb].h2[j] = __hmul2(batch_block[sb].h2[j], __float2half2_rn(1.f / 0x7fff));
+			}
 		}
-		*reinterpret_cast<ulong2*>(array_ptr + i) = batch_block.ul2;
+		for (unsigned sb = 0; sb < store_block_batch_size; sb++) {
+			*(reinterpret_cast<BLOCK_T*>(array_ptr + i) + sb) = batch_block[sb].store_block;
+		}
 	}
 	if (tid == 0) {
 		const auto res = size - batch_loop_size;
@@ -77,7 +90,7 @@ void mtk::curand_fp16::create(generator_t &gen, const curandRngType_t rng_type) 
 	gen.num_sm = prop.multiProcessorCount;
 
 	// calculate grid_size
-	gen.num_threads = gen.num_sm * 16 * block_size;
+	gen.num_threads = gen.num_sm * num_sm_scale * block_size;
 
 	// set algo
 	gen.rng_type = rng_type;
@@ -116,7 +129,7 @@ void mtk::curand_fp16::set_seed(generator_t &gen, const std::uint64_t seed) {
 void mtk::curand_fp16::uniform(generator_t &gen, half *const ptr, const std::size_t size, const bool pm) {
 	if (pm == 0) {
 		switch (gen.rng_type) {
-#define CASE_RNG_TYPE(rng) case rng: generate_kernel<typename mtk::curand_fp16::curand_status_t<rng>::type, 0>\
+#define CASE_RNG_TYPE(rng) case rng: generate_kernel<typename mtk::curand_fp16::curand_status_t<rng>::type, block_t, 0>\
 			<<<gen.num_threads / block_size, block_size, 0, gen.cuda_stream>>>\
 			(ptr, reinterpret_cast<typename mtk::curand_fp16::curand_status_t<rng>::type*>(gen.status_ptr), size);break
 			CASE_RNG_TYPE(CURAND_RNG_PSEUDO_MRG32K3A        );
@@ -128,7 +141,7 @@ void mtk::curand_fp16::uniform(generator_t &gen, half *const ptr, const std::siz
 		}
 	} else {
 		switch (gen.rng_type) {
-#define CASE_RNG_TYPE(rng) case rng: generate_kernel<typename mtk::curand_fp16::curand_status_t<rng>::type, 1>\
+#define CASE_RNG_TYPE(rng) case rng: generate_kernel<typename mtk::curand_fp16::curand_status_t<rng>::type, block_t, 1>\
 			<<<gen.num_threads / block_size, block_size, 0, gen.cuda_stream>>>\
 			(ptr, reinterpret_cast<typename mtk::curand_fp16::curand_status_t<rng>::type*>(gen.status_ptr), size);break
 			CASE_RNG_TYPE(CURAND_RNG_PSEUDO_MRG32K3A        );
